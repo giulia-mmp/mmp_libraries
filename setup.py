c2247ad070712db14aa5c07e1b218636f78b0acd
giulia <giulia.bortolato@milanomultiphysics.com>
2024-04-02 18:28:55 +0200

    Prova2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setuptools.setup(
     name='mmp_libraries',
     version='1.0.0',
-    packages=['mmp_libraries', 'oct2pyMMP', 'shieldingMMP'],  # setuptools.find_packages(),
+    packages=['mmp_libraries'],  #, 'oct2pyMMP', 'shieldingMMP'],  # setuptools.find_packages(),
     license='MIT',
     description='Custom libraries and utilities for MMP',
     long_description=long_description,
